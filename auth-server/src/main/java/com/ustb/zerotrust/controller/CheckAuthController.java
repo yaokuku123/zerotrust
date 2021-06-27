b026512a1ffd719e8a2ef188224814fb485090d4
wyp <834298243@qq.com>
2021-06-27 17:38:17 +0800

    modefied transferParam

--- auth-server/src/main/java/com/ustb/zerotrust/controller/CheckAuthController.java
+++ auth-server/src/main/java/com/ustb/zerotrust/controller/CheckAuthController.java
@@ -3,6 +3,7 @@ package com.ustb.zerotrust.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ustb.zerotrust.BlindVerify.Verify;
+import com.ustb.zerotrust.entity.QueryParam;
 import com.ustb.zerotrust.service.ChainClient;
 import com.ustb.zerotrust.service.DaemonClient;
 
@@ -48,6 +49,8 @@ public class CheckAuthController {
     @GetMapping("/result")
     public String checkResult() throws Exception {
 
+        QueryParam queryParam = new QueryParam();
+
         //查询 返回参数
         String s = daemonClient.getMessage();
         ConvertUtil convertUtil = new ConvertUtil();
@@ -55,9 +58,9 @@ public class CheckAuthController {
         HashMap<String, Object> hashMap = convertUtil.readfromJsonFile(fileName);
         //vLists sigma miuList signList
         String sigmaValues = (String)hashMap.get("sigmaValues");
-        JSONArray viStringList = (JSONArray) hashMap.get("viStringList");
-        JSONArray miuStringList = (JSONArray)hashMap.get("miuStringList");
-        JSONArray signStringList = (JSONArray)hashMap.get("signStringList");
+        List<String> viStringList = (List) hashMap.get("viStringList");
+        List<String> miuStringList = (List)hashMap.get("miuStringList");
+        List<String> signStringList = (List)hashMap.get("signStringList");
         log.info("sigmaValues :"+sigmaValues);
         log.info("viStringList :"+viStringList);
         log.info("miuStringList :"+miuStringList);
@@ -70,34 +73,17 @@ public class CheckAuthController {
         TypeACurveGenerator pg = new TypeACurveGenerator(rbits, qbits);
         PairingParameters typeAParams = pg.generate();
         Pairing pairing = PairingFactory.getPairing(typeAParams);
-        //初始化相关参数 还原
-
-//        Element g = pairing.getG1().newRandomElement().getImmutable();     //生成生成元
-//        Element x = pairing.getZr().newRandomElement().getImmutable();
-
-
+        queryParam.setPairing(pairing);
         Base64.Decoder decoder = Base64.getDecoder();
-        ArrayList<Element> viLists = new ArrayList<>();
-        ArrayList<Element> miuLists = new ArrayList<>();
-        ArrayList<Element> signLists = new ArrayList<>();
-        //List<> viStringLists = new ArrayList<>();
+        //初始化相关参数
 
-        for (Object elm : viStringList) {
-            byte[] signByte = decoder.decode(elm.toString().getBytes("UTF-8"));
-            viLists.add(pairing.getG1().newElementFromBytes(signByte).getImmutable());
-        }
+        ArrayList<Element> viLists = queryParam.decodeViLists(viStringList);
         log.info("viLists :"+viLists);
 
-        for (Object elm : miuStringList) {
-            byte[] signByte = decoder.decode(elm.toString().getBytes("UTF-8"));
-            miuLists.add(pairing.getG1().newElementFromBytes(signByte).getImmutable());
-        }
+        ArrayList<Element> miuLists = queryParam.decodeMiuLists(miuStringList);
         log.info("miuStringLists :"+miuLists);
 
-        for (Object elm : signStringList) {
-            byte[] signByte = decoder.decode(elm.toString().getBytes("UTF-8"));
-            signLists.add(pairing.getG1().newElementFromBytes(signByte).getImmutable());
-        }
+        ArrayList<Element> signLists = queryParam.decodeSignLists(signStringList);
         log.info("signLists :"+signLists);
 
         byte[] signByte = decoder.decode(sigmaValues.toString().getBytes("UTF-8"));
@@ -105,8 +91,8 @@ public class CheckAuthController {
         log.info("sigmasValues :"+sigmasValues);
 
 
-        //从链上获取参数 还原
-        JSONObject jsonObject = chainClient.getParam("typora-scrolls-0.5.zip");
+        //从链上获取参数
+        JSONObject jsonObject = chainClient.getParam("typora-scrolls-0.5");
 
         byte[] gEnByte = jsonObject.get("g").toString().getBytes();
         byte[] vEnByte = jsonObject.get("v").toString().getBytes();
@@ -117,25 +103,35 @@ public class CheckAuthController {
         byte[] gDeByte = decoder.decode(gEnByte);
         byte[] vDeByte = decoder.decode(vEnByte);
         Element g = pairing.getG1().newElementFromBytes(gDeByte);
-        Element x = pairing.getG1().newElementFromBytes(vDeByte);
+        Element v = pairing.getG1().newElementFromBytes(vDeByte);
+        log.info("g :"+g);
+        log.info("v :"+v);
         for(int i = 0; i< uStringList.size(); i++) {
             ElementPowPreProcessing u = pairing.getG1().newRandomElement().getField().getElementPowPreProcessingFromBytes(uStringList.get(i).getBytes());
             uLists.add(u);
         }
+        log.info("uLists :"+uLists);
 
-
-
-
-        //开始验证
+//        开始验证
 //        Element v = g.powZn(x);
 //        Verify verify = new Verify();
 //        boolean res = verify.verifyResult(pairing, g, uLists, v, sigmasValues, viLists, signLists, miuLists);
 //        System.out.println(res);
+//
+//        Verify verify = new Verify();
+//        res = verify.verifyResult(pairing, g, uList, v, sigmavalues, viList, signList, miuList);
 
-        return "ok";
+
+        return s;
     }
 
-
+//    @RequestMapping("/test")
+//    public String getParam(){
+//        String s = chainClient.getParam("typora-scrolls-0.5");
+//        System.out.println(s);
+//
+//        return s;
+//    }
 
 
 }
