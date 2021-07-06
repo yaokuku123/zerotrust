57d0f14779dc3793bdfa90376bdb031875425e42
yaokuku123 <yaoqijun@outlook.com>
2021-07-06 14:58:02 +0800

    Add global exception handler

---
+++ common/src/main/java/com/ustb/zerotrust/servicebase/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ustb.zerotrust.servicebase;
+
+import com.ustb.zerotrust.domain.ResponseCodeEnum;
+import com.ustb.zerotrust.domain.ResponseResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseResult error(Exception e){
+        e.printStackTrace();
+        return ResponseResult.error(ResponseCodeEnum.FAIL.getCode(),ResponseCodeEnum.FAIL.getMessage());
+    }
+}
