a52fe9fe0f2db0ada95611ec69ec52c8aa36a06a
wyp <834287243@qq.com>
2021-07-07 15:29:21 +0800

    Complete different types of software query

--- soft-test-center/src/main/java/com/ustb/zerotrust/controller/SoftReviewController.java
+++ soft-test-center/src/main/java/com/ustb/zerotrust/controller/SoftReviewController.java
@@ -1,12 +1,14 @@
 package com.ustb.zerotrust.controller;
 
 import com.ustb.zerotrust.domain.ResponseResult;
+import com.ustb.zerotrust.entity.vo.SoftSimpleInfoVo;
 import com.ustb.zerotrust.service.SoftReviewService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author WYP
@@ -20,27 +22,119 @@ public class SoftReviewController {
     @Autowired
     private SoftReviewService softReviewService;
 
+    @ApiOperation(value = "获取待处理软件个数",notes = "接口:/soft/PendingNums")
     @GetMapping("/PendingNums")
-    public ResponseResult<Integer> PendingNums(){
-        Integer nums = null;
+    public ResponseResult PendingNums(){
+
+        ResponseResult responseResult = null;
         try {
-            nums = softReviewService.PendingNums();
+            Integer nums = softReviewService.PendingNums();
+            responseResult = ResponseResult.success().data("nums",nums);
         } catch (Exception e) {
             e.printStackTrace();
+            responseResult = ResponseResult.error();
         }
-        return ResponseResult.success(nums);
+        return responseResult;
     }
 
+    @ApiOperation(value = "获取被驳回软件个数",notes = "接口:/soft/RejectNums")
     @GetMapping("/RejectNums")
-    public ResponseResult<Integer> RejectNums(){
-        Integer nums = null;
+    public ResponseResult RejectNums(){
+        ResponseResult responseResult = null;
         try {
-            nums = softReviewService.RejectNums();
+            Integer nums = softReviewService.RejectNums();
+            responseResult = ResponseResult.success().data("nums",nums);
         } catch (Exception e) {
             e.printStackTrace();
+            responseResult = ResponseResult.error();
         }
-        return ResponseResult.success(nums);
+        return responseResult;
     }
 
+    @ApiOperation(value = "获取已处理软件个数",notes = "接口:/soft/HandledNums")
+    @GetMapping("/HandledNums")
+    public ResponseResult HandledNums(){
+        ResponseResult responseResult = null;
+        try {
+            Integer nums = softReviewService.HandledNums();
+            responseResult = ResponseResult.success().data("nums",nums);
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseResult = ResponseResult.error();
+        }
+        return responseResult;
+    }
+
+
+    @ApiOperation(value = "获取所有已处理软件信息",notes = "接口:/soft/getAllPending")
+    @RequestMapping("/getAllPending")
+    public ResponseResult getAllPending(){
+        List<SoftSimpleInfoVo> allPending = null;
+        ResponseResult pendingSoft = null;
+        try {
+            allPending = softReviewService.getAllPending();
+            pendingSoft = ResponseResult.success().data("PendingSoft", allPending);
+        } catch (Exception e) {
+            e.printStackTrace();
+            pendingSoft = ResponseResult.error();
+        }
+        return pendingSoft;
+    }
+
+
+    @ApiOperation(value = "获取所有已驳回软件信息",notes = "接口:/soft/getAllReject")
+    @RequestMapping("/getAllReject")
+    public ResponseResult getAllReject(){
+        List<SoftSimpleInfoVo> allReject = null;
+        ResponseResult rejectSoft = null;
+        try {
+            allReject = softReviewService.getAllReject();
+            rejectSoft = ResponseResult.success().data("RejectSoft", allReject);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rejectSoft = ResponseResult.error();
+        }
+        return rejectSoft;
+    }
+
+
+    @ApiOperation(value = "获取所有已处理软件信息",notes = "接口:/soft/getAllHandled")
+    @RequestMapping("/getAllHandled")
+    public ResponseResult getAllHandled(){
+        List<SoftSimpleInfoVo> allHandled = null;
+        ResponseResult handledSoft = null;
+        try {
+            allHandled = softReviewService.getAllHandled();
+            handledSoft = ResponseResult.success().data("HandledSoft", allHandled);
+        } catch (Exception e) {
+            e.printStackTrace();
+            handledSoft = ResponseResult.error();
+        }
+        return handledSoft;
+    }
+
+    @ApiOperation(value = "审核通过待处理软件",notes = "接口:/soft/examineSoftSuccess")
+    @RequestMapping("/examineSoftSuccess")
+    public ResponseResult examineSoftSuccess(@RequestParam("id") int id){
+        try {
+            softReviewService.examineSoftSuccess(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseResult.error();
+        }
+        return ResponseResult.success();
+    }
+
+    @ApiOperation(value = "审核驳回处理软件",notes = "接口:/soft/examineSoftFail")
+    @RequestMapping("/examineSoftFail")
+    public ResponseResult examineSoftFail(@RequestParam("id") int id){
+        try {
+            softReviewService.examineSoftFail(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseResult.error();
+        }
+        return ResponseResult.success();
+    }
 
 }
--- soft-test-center/src/main/java/com/ustb/zerotrust/service/impl/SoftReviewServiceImpl.java
+++ soft-test-center/src/main/java/com/ustb/zerotrust/service/impl/SoftReviewServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ustb.zerotrust.service.impl;
 
+import com.ustb.zerotrust.entity.vo.SoftSimpleInfoVo;
 import com.ustb.zerotrust.mapper.SoftReviewDAO;
 import com.ustb.zerotrust.service.SoftReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author WYP
@@ -24,5 +28,35 @@ public class SoftReviewServiceImpl implements SoftReviewService {
     @Override
     public int RejectNums() {
         return softReviewDAO.RejectNums();
+    }
+
+    @Override
+    public int HandledNums() {
+        return softReviewDAO.HandledNums();
+    }
+
+    @Override
+    public List<SoftSimpleInfoVo> getAllPending() {
+        return softReviewDAO.getAllPending();
+    }
+
+    @Override
+    public List<SoftSimpleInfoVo> getAllReject() {
+        return softReviewDAO.getAllReject();
+    }
+
+    @Override
+    public List<SoftSimpleInfoVo> getAllHandled() {
+        return softReviewDAO.getAllHandled();
+    }
+
+    @Override
+    public void examineSoftSuccess(Integer id) {
+        softReviewDAO.examineSoftSuccess(id);
+    }
+
+    @Override
+    public void examineSoftFail(Integer id) {
+        softReviewDAO.examineSoftFail(id);
     }
 }
