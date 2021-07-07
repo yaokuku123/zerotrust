c171bc7ed1de2f5d92a5656e56c099f01caa9b37
wyp <834298243@qq.com>
2021-07-07 15:29:21 +0800

    Complete different types of software query

--- soft-test-center/src/main/java/com/ustb/zerotrust/mapper/SoftReviewDAO.java
+++ soft-test-center/src/main/java/com/ustb/zerotrust/mapper/SoftReviewDAO.java
@@ -1,6 +1,9 @@
 package com.ustb.zerotrust.mapper;
 
+import com.ustb.zerotrust.entity.vo.SoftSimpleInfoVo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * @author WYP
@@ -15,5 +18,22 @@ public interface SoftReviewDAO {
     //查询被驳回软件个数
     int RejectNums();
 
+    //查询已经办理软件个数
+    int HandledNums();
+
+    //获取待处理软件摘要信息
+    List<SoftSimpleInfoVo> getAllPending();
+
+    //获取被驳回软件摘要信息
+    List<SoftSimpleInfoVo> getAllReject();
+
+    //获取已经办理软件摘要信息
+    List<SoftSimpleInfoVo> getAllHandled();
+
+    //审核通过待处理软件
+    void examineSoftSuccess(Integer id);
+
+    //审核驳回待处理软件
+    void examineSoftFail(Integer id);
 
 }
--- soft-test-center/src/main/java/com/ustb/zerotrust/service/SoftReviewService.java
+++ soft-test-center/src/main/java/com/ustb/zerotrust/service/SoftReviewService.java
@@ -1,5 +1,9 @@
 package com.ustb.zerotrust.service;
 
+import com.ustb.zerotrust.entity.vo.SoftSimpleInfoVo;
+
+import java.util.List;
+
 /**
  * @author WYP
  * @date 2021-07-07 9:23
@@ -9,4 +13,16 @@ public interface SoftReviewService {
     int PendingNums();
 
     int RejectNums();
+
+    int HandledNums();
+
+    List<SoftSimpleInfoVo> getAllPending();
+
+    List<SoftSimpleInfoVo> getAllReject();
+
+    List<SoftSimpleInfoVo> getAllHandled();
+
+    void examineSoftSuccess(Integer id);
+
+    void examineSoftFail(Integer id);
 }
