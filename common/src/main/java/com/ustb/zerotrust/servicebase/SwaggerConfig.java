89e5bfa48c15115fb2289c3480eae549cb15acfe
yaokuku123 <yaoqijun@outlook.com>
2021-07-10 10:25:15 +0800

    modify some desc

--- common/src/main/java/com/ustb/zerotrust/servicebase/SwaggerConfig.java
+++ common/src/main/java/com/ustb/zerotrust/servicebase/SwaggerConfig.java
@@ -30,8 +30,8 @@ public class SwaggerConfig {
     private ApiInfo webApiInfo(){
 
         return new ApiInfoBuilder()
-                .title("网站-课程中心API文档")
-                .description("本文档描述了课程中心微服务接口定义")
+                .title("网站-软件认证服务API文档")
+                .description("本文档描述了软件认证微服务接口定义")
                 .version("1.0")
                 .contact(new Contact("Yorick", "http://yaoqijun.cn", "yaoqijun@outlook.com"))
                 .build();
