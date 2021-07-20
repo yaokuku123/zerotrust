79338849da7f54f3e8880be60238c3761d84b20b
wyp <834298243@qq.com>
2021-07-20 23:58:40 +0800

    modify check

--- auth-server/src/main/java/com/ustb/zerotrust/service/DaemonClient.java
+++ auth-server/src/main/java/com/ustb/zerotrust/service/DaemonClient.java
@@ -23,4 +23,7 @@ public interface DaemonClient {
     @RequestMapping("/GetCheckMessage")
     QueryParamString getMessage(@RequestParam("fileName") String fileName);
 
+    @RequestMapping("/GetCheckMessage1")
+    QueryParamString getMessage1(@RequestParam("fileName") String fileName);
+
 }
