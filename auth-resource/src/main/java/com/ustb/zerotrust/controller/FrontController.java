eb0715747979572f7169b66601b4c527b955989a
yaokuku123 <yaoqijun@outlook.com>
2021-12-09 09:19:32 +0800

    修改页面信息，成功跑通流程业务

--- auth-resource/src/main/java/com/ustb/zerotrust/controller/FrontController.java
+++ auth-resource/src/main/java/com/ustb/zerotrust/controller/FrontController.java
@@ -1,13 +1,21 @@
 package com.ustb.zerotrust.controller;
 
+import com.ustb.zerotrust.domain.ResponseResult;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin
 public class FrontController {
 
-    @RequestMapping("/front")
-    public String front() {
+    @PostMapping("/front")
+    @ResponseBody
+    public ResponseResult front() {
+        return ResponseResult.success();
+    }
+
+    @GetMapping("/front-get")
+    public String frontGet() {
         return "front";
     }
 }
